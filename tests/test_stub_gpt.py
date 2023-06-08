531784f06f66801bfadf1332eba1844c4bc9ba8c
Matt Clarke <matt.clarke@ess.eu>
2023-06-08 14:24:27 +0200

    Added tests for when we go first and the bot goes first

--- tests/test_stub_gpt.py
+++ tests/test_stub_gpt.py
@@ -6,7 +6,7 @@ class StubGptWrapper:
     def __init__(self):
         self.messages = []
 
-    def send(prompt):
+    def send(self, prompt):
         return self.messages.pop(0)
 
 
@@ -39,9 +39,8 @@ class Game:
                 "where 1 is the top left corner and 9 is the bottom right corner."
                 "You play is X and i play as O. You go first"
             )
-        # TODO: parse response
 
-        return [None] * 9
+        return self.extract_board(response)[-1]
 
 
 def test_parse_initial_response_when_we_go_first():
@@ -93,37 +92,55 @@ def test_parse_initial_response_when_bot_goes_first():
     assert boards[1] == ["1", "2", "3", "4", "X", "6", "7", "8", "9"]
 
 
-#
-# def test_asking_it_to_start_a_new_game():
-#     # Ask to start a game and we go first
-#     new_game_response = {
-#         "choices": [
-#             {
-#                 "finish_reason": "stop",
-#                 "index": 0,
-#                 "message": {
-#                     "content": "Sure, I'll go ahead and play with you. Here's the board:\n\n```\n1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9\n```\n\nOK, your move first. Which number would you like to place your 'O' in?",
-#                     "role": "assistant",
-#                 },
-#             }
-#         ],
-#         "created": 1685704835,
-#         "id": "chatcmpl-7Mx7btPzM9im7tTUaqZMhnurz8o5e",
-#         "model": "gpt-3.5-turbo-0301",
-#         "object": "chat.completion",
-#         "usage": {"completion_tokens": 65, "prompt_tokens": 76, "total_tokens": 141},
-#     }
-#
-#     client = StubGptWrapper()
-#     client.messages.append(new_game_response)
-#     game = Game(client)
-#     current_board = game.start(player_first=True)
-#
-#     assert current_board == [None] * 9
-#
-#
-# def test_new_game_bot_goes_first():
-#     game = Game()
-#     current_board = game.start(player_first=False)
-#
-#     assert current_board == [None] * 9
+def test_asking_it_to_start_a_new_game_we_go_first():
+    new_game_response = {
+        "choices": [
+            {
+                "finish_reason": "stop",
+                "index": 0,
+                "message": {
+                    "content": "Sure, I'll go ahead and play with you. Here's the board:\n\n```\n1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9\n```\n\nOK, your move first. Which number would you like to place your 'O' in?",
+                    "role": "assistant",
+                },
+            }
+        ],
+        "created": 1685704835,
+        "id": "chatcmpl-7Mx7btPzM9im7tTUaqZMhnurz8o5e",
+        "model": "gpt-3.5-turbo-0301",
+        "object": "chat.completion",
+        "usage": {"completion_tokens": 65, "prompt_tokens": 76, "total_tokens": 141},
+    }
+
+    client = StubGptWrapper()
+    client.messages.append(new_game_response)
+    game = Game(client)
+    current_board = game.start(player_first=True)
+
+    assert current_board == ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+def test_asking_it_to_start_a_new_game_bot_goes_first():
+    new_game_response = {
+        "choices": [
+            {
+                "finish_reason": "stop",
+                "index": 0,
+                "message": {
+                    "content": "Sure thing! \n\nHere's the initial board:\n\n```\n1 | 2 | 3  \n---------\n4 | 5 | 6 \n---------\n7 | 8 | 9\n```\n\nI'll start by placing an X in the center of the board (position 5):\n\n```\n1 | 2 | 3  \n---------\n4 | X | 6 \n---------\n7 | 8 | 9\n```\n\nYour turn, where would you like to put your O?",
+                    "role": "assistant",
+                },
+            }
+        ],
+        "created": 1685704217,
+        "id": "chatcmpl-7MwxdUK2s5EmQ1uS86YlqI0kLE9vU",
+        "model": "gpt-3.5-turbo-0301",
+        "object": "chat.completion",
+        "usage": {"completion_tokens": 99, "prompt_tokens": 76, "total_tokens": 175},
+    }
+
+    client = StubGptWrapper()
+    client.messages.append(new_game_response)
+    game = Game(client)
+    current_board = game.start(player_first=False)
+
+    assert current_board == ["1", "2", "3", "4", "X", "6", "7", "8", "9"]
