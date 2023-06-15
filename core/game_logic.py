b4b61ed152c1fa76dbe5dceb767b8dcd8d7fb11b
Jonas Petersson <jonas.petersson@ess.eu>
2023-06-15 09:40:31 +0200

    adding checks for allowed moves

--- core/game_logic.py
+++ core/game_logic.py
@@ -1,14 +1,29 @@
+from enum import Enum
 
 
+class SquareState(Enum):
+    EMPTY = 0
+    X = 1
+    O = 2
+
+
+class InvalidMoveException(Exception):
+    pass
 
 class Board:
     def __init__(self):
-        self.board = [""]*9
-        self.current_player = "O"
+        self.board = [SquareState.EMPTY]*9
+        self.current_player = SquareState.O
 
     def is_empty(self):
-        return all([square == "" for square in self.board])
+        return all([square == SquareState.EMPTY for square in self.board])
 
     def make_move(self, position):
+        if position < 0 or position > 8:
+            raise InvalidMoveException("Position is outside of board")
+
+        if self.board[position] != SquareState.EMPTY:
+            raise InvalidMoveException("Square is already taken")
+
         self.board[position] = self.current_player
-        self.current_player = "X" if self.current_player == "O" else "O"
+        self.current_player = SquareState.X if self.current_player == SquareState.O else SquareState.O
--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -1,26 +1,60 @@
-from core.game_logic import Board
+from core.game_logic import Board, SquareState
+import pytest
 
-
-
-
+from core.game_logic import InvalidMoveException
 
 
 def test_empty_board_on_init():
     board = Board()
     assert board.is_empty()
 
+
 def test_that_first_move_is_o():
     board = Board()
     board.make_move(0)
-    assert board.board == ["O", "", "", "", "", "", "", "", ""]
+    assert board.board == [
+        SquareState.O,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+    ]
+
 
 def test_second_move_is_x():
     board = Board()
     board.make_move(0)
     board.make_move(1)
-    assert board.board == ["O", "X", "", "", "", "", "", "", ""]
+    assert board.board == [
+        SquareState.O,
+        SquareState.X,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+        SquareState.EMPTY,
+    ]
 
 
+def test_not_allowed_move_to_same_square():
+    board = Board()
+    board.make_move(0)
+    with pytest.raises(InvalidMoveException):
+        board.make_move(0)
 
+
+def test_not_allowed_move_outside_of_board():
+    board = Board()
+    with pytest.raises(InvalidMoveException):
+        board.make_move(9)
+
+    with pytest.raises(InvalidMoveException):
+        board.make_move(-1)
 
 
