da86c4c599d7007e14a5fc87248357ba312f4e8e
Jonas Petersson <jonas.petersson@ess.eu>
2023-06-22 09:38:00 +0200

    cleaning up and adding board factory

--- core/game_logic.py
+++ core/game_logic.py
@@ -1,5 +1,16 @@
 from enum import Enum
 
+WINNING_COMBOS = [
+    (0, 1, 2),
+    (3, 4, 5),
+    (6, 7, 8),
+    (0, 3, 6),
+    (1, 4, 7),
+    (2, 5, 8),
+    (0, 4, 8),
+    (2, 4, 6),
+]
+
 
 class SquareState(Enum):
     EMPTY = 0
@@ -10,16 +21,17 @@ class SquareState(Enum):
 class Result(Enum):
     NOT_FINISHED = 0
     DRAW = 1
-    X = 2
-    O = 3
+    X_WINS = 2
+    O_WINS = 3
 
 
 class InvalidMoveException(Exception):
     pass
 
+
 class Board:
     def __init__(self):
-        self.board = [SquareState.EMPTY]*9
+        self.board = [SquareState.EMPTY] * 9
         self.current_player = SquareState.O
 
     def is_empty(self):
@@ -33,23 +45,25 @@ class Board:
             raise InvalidMoveException("Square is already taken")
 
         self.board[position] = self.current_player
-        self.current_player = SquareState.X if self.current_player == SquareState.O else SquareState.O
+        self.current_player = (
+            SquareState.X if self.current_player == SquareState.O else SquareState.O
+        )
 
     def _is_win(self):
-        winning_combos = [
-            (0, 1, 2), (3, 4, 5), (6, 7, 8),
-            (0, 3, 6), (1, 4, 7), (2, 5, 8),
-            (0, 4, 8), (2, 4, 6),
-        ]
-
-        return any([self.board[a] == self.board[b] == self.board[c] for a, b, c in winning_combos if self.board[a] != SquareState.EMPTY])
+        return any(
+            [
+                self.board[a] == self.board[b] == self.board[c]
+                for a, b, c in WINNING_COMBOS
+                if self.board[a] != SquareState.EMPTY
+            ]
+        )
 
     def get_available_moves(self):
         return [i for i, square in enumerate(self.board) if square == SquareState.EMPTY]
 
     def get_result(self):
         if self._is_win():
-            return Result.X if self.current_player == SquareState.O else Result.O
+            return Result.X_WINS if self.current_player == SquareState.O else Result.O_WINS
         elif len(self.get_available_moves()) == 0:
             return Result.DRAW
         else:
--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -4,13 +4,30 @@ import pytest
 from core.game_logic import InvalidMoveException
 
 
-def test_empty_board_on_init():
+def create_board(state=None):
+    if not state:
+        return Board()
+
     board = Board()
+    o_idx = [i for i, x in enumerate(state) if x == "O"]
+    x_idx = [i for i, x in enumerate(state) if x == "X"]
+
+    for i in range(len(o_idx) + len(x_idx)):
+        if i % 2 == 0:
+            board.make_move(o_idx.pop())
+        else:
+            board.make_move(x_idx.pop())
+
+    return board
+
+
+def test_empty_board_on_init():
+    board = create_board()
     assert board.is_empty()
 
 
 def test_that_first_move_is_o():
-    board = Board()
+    board = create_board()
     board.make_move(0)
     assert board.board == [
         SquareState.O,
@@ -26,7 +43,7 @@ def test_that_first_move_is_o():
 
 
 def test_second_move_is_x():
-    board = Board()
+    board = create_board()
     board.make_move(0)
     board.make_move(1)
     assert board.board == [
@@ -43,111 +60,78 @@ def test_second_move_is_x():
 
 
 def test_not_allowed_move_to_same_square():
-    board = Board()
+    board = create_board()
     board.make_move(0)
     with pytest.raises(InvalidMoveException):
         board.make_move(0)
 
 
-def test_not_allowed_move_outside_of_board():
-    board = Board()
+def test_not_allowed_move_outside_of_create_board():
+    board = create_board()
     with pytest.raises(InvalidMoveException):
         board.make_move(9)
-
     with pytest.raises(InvalidMoveException):
         board.make_move(-1)
 
 
 def test_that_game_not_finished():
-    board = Board()
-    board.make_move(0)
-    board.make_move(3)
-    board.make_move(1)
-    board.make_move(4)
+    board = create_board(
+        ['O', 'O', '',
+         'X', 'X', '',
+         '', '', '']
+    )
     assert board.get_result() == Result.NOT_FINISHED
 
 
 def test_that_three_in_a_row_is_win():
-    board = Board()
-    board.make_move(0)
-    board.make_move(3)
-    board.make_move(1)
-    board.make_move(4)
-    board.make_move(2)
-    assert board.get_result() != Result.DRAW
+    board = create_board(
+        ['O', 'O', 'O',
+         'X', 'X', '',
+         '', '', '']
+    )
+    assert board.get_result() == Result.O_WINS
+
 
 def test_that_three_in_a_col_is_win():
-    board = Board()
-    board.make_move(2)
-    board.make_move(4)
-    board.make_move(5)
-    board.make_move(6)
-    board.make_move(8)
-    assert board.get_result() != Result.DRAW
+    board = create_board(
+        ['', '', 'O',
+         '', 'X', 'O',
+         'X', '', 'O']
+    )
+    assert board.get_result() == Result.O_WINS
 
 
 def test_that_three_in_a_diag_is_win():
-    board = Board()
-    board.make_move(2)
-    board.make_move(0)
-    board.make_move(4)
-    board.make_move(8)
-    board.make_move(6)
-    assert board.get_result() != Result.DRAW
+    board = create_board(
+        ['', 'X', 'O',
+         '', 'O', 'X',
+         'O', '', '']
+    )
+    assert board.get_result() == Result.O_WINS
 
 
 def test_that_game_is_finished_after_winning():
-    board = Board()
-    board.make_move(2)
-    board.make_move(0)
-    board.make_move(4)
-    board.make_move(8)
-    board.make_move(6)
+    board = create_board(
+        ['O', 'O', 'O',
+         'X', 'X', '',
+         '', '', '']
+    )
     assert board.get_result() != Result.NOT_FINISHED
 
 
 def test_that_game_is_draw_after_all_square_are_taken():
-    board = Board()
-    board.make_move(0)
-    board.make_move(4)
-    board.make_move(2)
-    board.make_move(1)
-    board.make_move(7)
-    board.make_move(6)
-    board.make_move(5)
-    board.make_move(8)
-    board.make_move(3)
+    board = create_board(
+        ['O', 'X', 'O',
+         'O', 'X', 'X',
+         'X', 'O', 'O']
+    )
     assert board.get_result() == Result.DRAW
 
 
-def test_first_player_won_the_game():
-    board = Board()
-    board.make_move(2)
-    board.make_move(0)
-    board.make_move(4)
-    board.make_move(8)
-    board.make_move(6)
-    assert board.get_result() == Result.O
-
-
 def test_second_player_won_the_game():
-    board = Board()
-    board.make_move(7)
-    board.make_move(2)
-    board.make_move(0)
-    board.make_move(4)
-    board.make_move(8)
-    board.make_move(6)
-    assert board.get_result() == Result.X
-
-
-def test_get_result_when_game_is_not_finished():
-    board = Board()
-    board.make_move(7)
-    board.make_move(2)
-    board.make_move(0)
-    board.make_move(4)
-    board.make_move(8)
-    assert board.get_result() == Result.NOT_FINISHED
-
-
+    board = create_board(
+        ['O', 'O', '',
+         'X', 'X', 'X',
+         '', '', 'O']
+    )
+    assert board.get_result() == Result.X_WINS
